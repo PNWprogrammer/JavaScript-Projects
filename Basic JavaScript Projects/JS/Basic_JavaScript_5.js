3a2437051921dbc02417c7487462e8cf50ae33b2
Austin <60404953+PNWprogrammer@users.noreply.github.com>
2020-02-18 12:12:44 -0800

    Assignment 5
    NOT operator to display true and false.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,7 @@
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (8 > 10);
+}
+
+function not_Function2() {
+    document.getElementById("Not2").innerHTML = ! (15 > 10);
+}

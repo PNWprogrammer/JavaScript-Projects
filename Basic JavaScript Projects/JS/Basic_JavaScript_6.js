7d79c8b281f3c9221c7ddd08eb44950a8d6e0242
Austin <60404953+PNWprogrammer@users.noreply.github.com>
2020-02-18 13:49:39 -0800

    Changes mad to HTMl_5 and JavaScript_5 and added HTML_6 and JavaScript_6

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_6.js
@@ -0,0 +1,33 @@
+function Vote_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "Sorry, you are not":"Congrats, you are";
+    document.getElementById("Vote").innerHTML = Can_vote + " old enough to vote.";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", "2020", "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", "2019", "White");
+var Erik = new Vehicle("Ford", "Pinto", "1971", "Mustard");
+function my_Function() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+
+function House(Style, Year, Color){
+    this.House_Style = Style;
+    this.House_Year = Year;
+    this.House_Color = Color;
+}
+
+var George = new House("Farm", "1984", "Red");
+var Ben = new House("Modern", "2020", "Slate");
+var Abe = new House("Colonial", "1850", "White");
+function House_Function() {
+    document.getElementById("New_and_This").innerHTML = "This house is owned by Abe. It was built in " + Abe.House_Year + " it is a " + Abe.House_Style + " style home " + " it is painted " + Abe.House_Color;
+}

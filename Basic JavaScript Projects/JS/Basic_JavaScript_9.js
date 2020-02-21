76227c475a259889515f1ec77a604974130b9733
Austin <60404953+PNWprogrammer@users.noreply.github.com>
2020-02-21 13:09:36 -0800

    Timer added to HTML_9

--- Basic JavaScript Projects/JS/Basic_JavaScript_9.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9.js
@@ -26,3 +26,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+function countdown() {
+  var seconds = document.getElementById("seconds").value;
+
+  function tick() {
+    seconds = seconds - 1;
+    TimeRanges.innerHTML = seconds;
+    setTimeout(tick,1000);
+  if(seconds == -1) {
+    alert("Time is up!");
+  }
+    }
+  tick();
+  
+}
